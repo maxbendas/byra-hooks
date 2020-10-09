fc9fb15c4a8eaca606da7de85b6f2d1c2a80de87
maxbendas <maxbendas86@gmail.com>
2020-10-09 15:55:22 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    return (
+        <div>
+            <HookSwitcher/>
+        </div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const HookSwitcher = () => {
+
+    const [color, setColor] = useState('gray')
+    const [fontSize, setFontSize] = useState(14)
+
+    return (
+        <div style={{padding: '10px', backgroundColor: color, fontSize: `${fontSize}px`}}>
+            Hello
+            <button onClick={() => {
+                setColor('gray')
+            }}>Dark
+            </button>
+            <button onClick={() => {
+                setColor('white')
+            }}>Light
+            </button>
+            <button onClick={() => {
+                setFontSize((s)=>s+2)
+            }}>+
+            </button>
+        </div>
+    )
+}
+
+ReactDOM.render(<   App / >, document.getElementById('root'))    ;
+
+
